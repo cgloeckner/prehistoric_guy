04dff36185040c2be12803c020b27ddba86dfd09
cgloeckner <cgloeckner@freenet.de>
2023-04-06 14:00:57 +0200

    imgui wrapper refactored

--- imgui_wrapper.py
+++ imgui_wrapper.py
@@ -8,22 +8,18 @@ Based on
     URL: https://stackoverflow.com/questions/61396799/how-can-i-blit-my-pygame-game-onto-an-opengl-surface
 """
 
-from imgui.integrations.opengl import FixedPipelineRenderer
-
 import pygame
-import pygame.event
-import pygame.time
 
 import imgui
+from imgui.integrations.opengl import FixedPipelineRenderer
 
-import sys
-
-import pygame
-import OpenGL.GL as gl
-
+import OpenGL.GL
 
 
 class PygameRenderer(FixedPipelineRenderer):
+    """
+    Based on https://github.com/pyimgui/pyimgui/blob/dev/version-2.0/imgui/integrations/pygame.py
+    """
     def __init__(self):
         super(PygameRenderer, self).__init__()
 
@@ -33,8 +29,8 @@ class PygameRenderer(FixedPipelineRenderer):
         self._map_keys()
 
     def _custom_key(self, key):
-        # We need to go to custom keycode since imgui only support keycod from 0..512 or -1
-        if not key in self.custom_key_map:
+        # We need to go to custom keycode since imgui only support keycode from 0..512 or -1
+        if key not in self.custom_key_map:
             self.custom_key_map[key] = len(self.custom_key_map)
         return self.custom_key_map[key]
 
@@ -64,48 +60,48 @@ class PygameRenderer(FixedPipelineRenderer):
         key_map[imgui.KEY_Y] = self._custom_key(pygame.K_y)
         key_map[imgui.KEY_Z] = self._custom_key(pygame.K_z)
 
-    def process_event(self, event):
+    def process_event(self, ev):
         # perf: local for faster access
         io = self.io
 
-        if event.type == pygame.MOUSEMOTION:
-            io.mouse_pos = event.pos
+        if ev.type == pygame.MOUSEMOTION:
+            io.mouse_pos = ev.pos
             return True
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+            if ev.button == 1:
                 io.mouse_down[0] = 1
-            if event.button == 2:
+            if ev.button == 2:
                 io.mouse_down[1] = 1
-            if event.button == 3:
+            if ev.button == 3:
                 io.mouse_down[2] = 1
             return True
 
-        if event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
+        if ev.type == pygame.MOUSEBUTTONUP:
+            if ev.button == 1:
                 io.mouse_down[0] = 0
-            if event.button == 2:
+            if ev.button == 2:
                 io.mouse_down[1] = 0
-            if event.button == 3:
+            if ev.button == 3:
                 io.mouse_down[2] = 0
-            if event.button == 4:
+            if ev.button == 4:
                 io.mouse_wheel = .5
-            if event.button == 5:
+            if ev.button == 5:
                 io.mouse_wheel = -.5
             return True
 
-        if event.type == pygame.KEYDOWN:
-            for char in event.unicode:
+        if ev.type == pygame.KEYDOWN:
+            for char in ev.unicode:
                 code = ord(char)
                 if 0 < code < 0x10000:
                     io.add_input_character(code)
 
-            io.keys_down[self._custom_key(event.key)] = True
+            io.keys_down[self._custom_key(ev.key)] = True
 
-        if event.type == pygame.KEYUP:
-            io.keys_down[self._custom_key(event.key)] = False
+        if ev.type == pygame.KEYUP:
+            io.keys_down[self._custom_key(ev.key)] = False
 
-        if event.type in (pygame.KEYDOWN, pygame.KEYUP):
+        if ev.type in (pygame.KEYDOWN, pygame.KEYUP):
             io.key_ctrl = (
                     io.keys_down[self._custom_key(pygame.K_LCTRL)] or
                     io.keys_down[self._custom_key(pygame.K_RCTRL)]
@@ -128,19 +124,19 @@ class PygameRenderer(FixedPipelineRenderer):
 
             return True
 
-        if event.type == pygame.VIDEORESIZE:
+        if ev.type == pygame.VIDEORESIZE:
             surface = pygame.display.get_surface()
             # note: pygame does not modify existing surface upon resize,
             #       we need to to it ourselves.
             pygame.display.set_mode(
-                (event.w, event.h),
+                (ev.w, ev.h),
                 flags=surface.get_flags(),
             )
             # existing font texure is no longer valid, so we need to refresh it
             self.refresh_font_texture()
 
             # notify imgui about new window size
-            io.display_size = event.size
+            io.display_size = ev.size
 
             # delete old surface, it is no longer needed
             del surface
@@ -156,72 +152,108 @@ class PygameRenderer(FixedPipelineRenderer):
             io.delta_time = current_time - self._gui_time
         else:
             io.delta_time = 1. / 60.
-        if (io.delta_time <= 0.0): io.delta_time = 1. / 1000.
+        if io.delta_time <= 0.0:
+            io.delta_time = 1. / 1000.
         self._gui_time = current_time
 
 
-import OpenGL.GL as gl
+class OpenGlWrapper(object):
+    """
+    Based on: https://stackoverflow.com/questions/61396799/how-can-i-blit-my-pygame-game-onto-an-opengl-surface
+    and https://github.com/pyimgui/pyimgui/blob/master/doc/examples/integrations_pygame.py
 
-texID = 0
+    Usage:
+    (1) Create a pygame display using the flags from `get_display.flags()`
+    (2) Create a wrapper instance from that screen
+    (3) Forward events to this wrapper using `process_event()`
+    (4) Clear and blit on the buffer provided by wrapper_instance.buffer
+    (5) Create the ImGui widgets as usual
+    (6) Render everything onto the OpenGL screen using `render()`
+
+    The internal rendering order is: buffer first, imgui second.
+    """
+
+    def __init__(self, main_screen: pygame.Surface):
+        """Initialize OpenGL and ImGui.
+        """
+        self.screen = main_screen
+        self.size = screen.get_size()
+        self.buffer = pygame.Surface(self.size)
+
+        # Setup OpenGL Texture
+        OpenGL.GL.glViewport(0, 0, *self.size)
+        OpenGL.GL.glDepthRange(0, 1)
+        OpenGL.GL.glMatrixMode(OpenGL.GL.GL_PROJECTION)
+        OpenGL.GL.glMatrixMode(OpenGL.GL.GL_MODELVIEW)
+        OpenGL.GL.glLoadIdentity()
+        OpenGL.GL.glShadeModel(OpenGL.GL.GL_SMOOTH)
+        OpenGL.GL.glClearColor(0.0, 0.0, 0.0, 0.0)
+        OpenGL.GL.glClearDepth(1.0)
+        OpenGL.GL.glDisable(OpenGL.GL.GL_DEPTH_TEST)
+        OpenGL.GL.glDisable(OpenGL.GL.GL_LIGHTING)
+        OpenGL.GL.glDepthFunc(OpenGL.GL.GL_LEQUAL)
+        OpenGL.GL.glHint(OpenGL.GL.GL_PERSPECTIVE_CORRECTION_HINT, OpenGL.GL.GL_NICEST)
+        OpenGL.GL.glEnable(OpenGL.GL.GL_BLEND)
+        self.tex_id = OpenGL.GL.glGenTextures(1)
+
+        # setup ImGui stuff
+        imgui.create_context()
+        self.impl = PygameRenderer()
+        self.io = imgui.get_io()
+        self.io.display_size = self.size
+
+    @staticmethod
+    def get_display_flags() -> int:
+        """Returns the required flags for the pygame display init.
+        """
+        return pygame.DOUBLEBUF | pygame.OPENGL | pygame.RESIZABLE
+
+    def process_event(self, ev: pygame.event.Event) -> None:
+        """Forwards events to ImGui.
+        """
+        self.impl.process_event(ev)
+
+    def render(self) -> None:
+        """Transforms the given surface into a texture, renders it and draws ImGui widgets on top.
+        """
+        # upload buffer to texture
+        rgb_surface = pygame.image.tostring(self.buffer, 'RGB')
+        OpenGL.GL.glBindTexture(OpenGL.GL.GL_TEXTURE_2D, self.tex_id)
+        OpenGL.GL.glTexParameteri(OpenGL.GL.GL_TEXTURE_2D, OpenGL.GL.GL_TEXTURE_MAG_FILTER, OpenGL.GL.GL_NEAREST)
+        OpenGL.GL.glTexParameteri(OpenGL.GL.GL_TEXTURE_2D, OpenGL.GL.GL_TEXTURE_MIN_FILTER, OpenGL.GL.GL_NEAREST)
+        OpenGL.GL.glTexParameteri(OpenGL.GL.GL_TEXTURE_2D, OpenGL.GL.GL_TEXTURE_WRAP_S, OpenGL.GL.GL_CLAMP)
+        OpenGL.GL.glTexParameteri(OpenGL.GL.GL_TEXTURE_2D, OpenGL.GL.GL_TEXTURE_WRAP_T, OpenGL.GL.GL_CLAMP)
+        OpenGL.GL.glTexImage2D(OpenGL.GL.GL_TEXTURE_2D, 0, OpenGL.GL.GL_RGB, *self.size, 0, OpenGL.GL.GL_RGB,
+                               OpenGL.GL.GL_UNSIGNED_BYTE, rgb_surface)
+        OpenGL.GL.glGenerateMipmap(OpenGL.GL.GL_TEXTURE_2D)
+        OpenGL.GL.glBindTexture(OpenGL.GL.GL_TEXTURE_2D, 0)
+
+        # clear OpenGL screen
+        OpenGL.GL.glClear(OpenGL.GL.GL_COLOR_BUFFER_BIT)
+        OpenGL.GL.glLoadIdentity()
+        OpenGL.GL.glDisable(OpenGL.GL.GL_LIGHTING)
+        OpenGL.GL.glEnable(OpenGL.GL.GL_TEXTURE_2D)
+
+        # draw texture onto OpenGL screen
+        OpenGL.GL.glBindTexture(OpenGL.GL.GL_TEXTURE_2D, self.tex_id)
+        OpenGL.GL.glBegin(OpenGL.GL.GL_QUADS)
+        OpenGL.GL.glTexCoord2f(0, 0)
+        OpenGL.GL.glVertex2f(-1, 1)
+        OpenGL.GL.glTexCoord2f(0, 1)
+        OpenGL.GL.glVertex2f(-1, -1)
+        OpenGL.GL.glTexCoord2f(1, 1)
+        OpenGL.GL.glVertex2f(1, -1)
+        OpenGL.GL.glTexCoord2f(1, 0)
+        OpenGL.GL.glVertex2f(1, 1)
+        OpenGL.GL.glEnd()
+
+        # render imgui stuff to OpenGL screen
+        imgui.render()
+        self.impl.render(imgui.get_draw_data())
 
 
-def init(size):
-    # basic opengl configuration
-    gl.glViewport(0, 0, *size)
-    gl.glDepthRange(0, 1)
-    gl.glMatrixMode(gl.GL_PROJECTION)
-    gl.glMatrixMode(gl.GL_MODELVIEW)
-    gl.glLoadIdentity()
-    gl.glShadeModel(gl.GL_SMOOTH)
-    gl.glClearColor(0.0, 0.0, 0.0, 0.0)
-    gl.glClearDepth(1.0)
-    gl.glDisable(gl.GL_DEPTH_TEST)
-    gl.glDisable(gl.GL_LIGHTING)
-    gl.glDepthFunc(gl.GL_LEQUAL)
-    gl.glHint(gl.GL_PERSPECTIVE_CORRECTION_HINT, gl.GL_NICEST)
-    gl.glEnable(gl.GL_BLEND)
-
-    texID = gl.glGenTextures(1)
-
-
-def surfaceToTexture(texID, pygame_surface):
-    rgb_surface = pygame.image.tostring(pygame_surface, 'RGB')
-    gl.glBindTexture(gl.GL_TEXTURE_2D, texID)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP)
-    surface_rect = pygame_surface.get_rect()
-    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB, surface_rect.width, surface_rect.height, 0, gl.GL_RGB,
-                    gl.GL_UNSIGNED_BYTE, rgb_surface)
-    gl.glGenerateMipmap(gl.GL_TEXTURE_2D)
-    gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
-
-
-def main():
-    pygame.init()
-    size = 800, 600
-
-    screen = pygame.display.set_mode(size, pygame.DOUBLEBUF | pygame.OPENGL | pygame.RESIZABLE)
-    init(size)
-
-    buffer = pygame.Surface(size)
-
-    imgui.create_context()
-    impl = PygameRenderer()
-
-    io = imgui.get_io()
-    io.display_size = size
-    
-    background = pygame.image.load('data/background.png')
-
-    while 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-            impl.process_event(event)
-
+if __name__ == "__main__":
+    def demo_ui():
         imgui.new_frame()
 
         if imgui.begin_main_menu_bar():
@@ -244,41 +276,31 @@ def main():
         imgui.text_colored("Eggs", 0.2, 1., 0.)
         imgui.end()
 
-        buffer.fill('black')
-        buffer.blit(background, (0, 0))
+    pygame.init()
+    size = 800, 600
 
-        # prepare to render the texture-mapped rectangle
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-        gl.glLoadIdentity()
-        gl.glDisable(gl.GL_LIGHTING)
-        gl.glEnable(gl.GL_TEXTURE_2D)
-        # glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        # glClearColor(0, 0, 0, 1.0)
+    screen = pygame.display.set_mode(size, OpenGlWrapper.get_display_flags())
+    wrapper = OpenGlWrapper(screen)
 
-        # draw texture openGL Texture
-        surfaceToTexture(texID, buffer)
-        gl.glBindTexture(gl.GL_TEXTURE_2D, texID)
-        gl.glBegin(gl.GL_QUADS)
-        gl.glTexCoord2f(0, 0);
-        gl.glVertex2f(-1, 1)
-        gl.glTexCoord2f(0, 1);
-        gl.glVertex2f(-1, -1)
-        gl.glTexCoord2f(1, 1);
-        gl.glVertex2f(1, -1)
-        gl.glTexCoord2f(1, 0);
-        gl.glVertex2f(1, 1)
-        gl.glEnd()
+    background = pygame.image.load('data/background.png')
 
-        # note: cannot use screen.fill((1, 1, 1)) because pygame's screen
-        #       does not support fill() on OpenGL sufraces
-        # gl.glClearColor(1, 1, 1, 1)
-        # gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-        imgui.render()
-        impl.render(imgui.get_draw_data())
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
 
-        screen.blit(buffer, (0, 0))
+            wrapper.process_event(event)
+
+        demo_ui()
+
+        # regular pygame blit etc.
+        wrapper.buffer.fill('black')
+        wrapper.buffer.blit(background, (0, 0))
+
+        # draws buffer and ImGui to screen
+        wrapper.render()
+
+        # as usual
         pygame.display.flip()
-
-
-if __name__ == "__main__":
-    main()
